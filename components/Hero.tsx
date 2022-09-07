1b3cdb45a549a6524b6861206555a611e4144650
John Running <johnrunning@Johns-MacBook-Pro.local>
2022-09-06 19:00:28 -0700

    messing around with component parameters to try to see why enhancer is failing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,21 +2,32 @@ import Link from "next/link";
 import { ComponentProps } from "@uniformdev/canvas-react";
 import Splitter from "./Splitter";
 
-type HeroProps = ComponentProps<{
-  title: string;
-  text: string;
-  buttonText: string;
-  buttonLink: string;
-  image?: string;
-}>;
+// type HeroProps = ComponentProps<{
+//   title: string;
+//   text: string;
+//   buttonText: string;
+//   buttonLink: string;
+//   image?: string;
+// }>;
 
-export function Hero({
-  title,
-  text,
-  buttonText,
-  image,
-  buttonLink,
-}: HeroProps) {
+// export function Hero({
+//   title,
+//   text,
+//   buttonText,
+//   image,
+//   buttonLink,
+// }: HeroProps) {
+
+export const Hero: React.FC<any> = ({ component }) => {
+  const entry = component?.parameters?.entry?.value;
+  // const { verticalTitle, header, shortCopy, image } = entry || {};
+  // const { src, alt } = image || {};
+  // const imageSrc = encodeURIComponent(src);
+  const theTitle = component.parameters.title.value;
+  console.log("parameters....", JSON.stringify(component.parameters, null, 2));
+  const buttonText = entry?.buttonText?.value;
+  const buttonLink = entry?.buttonLink?.value;
+
   return (
     <>
       <div className="pt-24">
@@ -27,11 +38,11 @@ export function Hero({
             </p>
             <h1
               className="my-4 text-5xl font-bold leading-tight"
-              dangerouslySetInnerHTML={{ __html: title }}
+              dangerouslySetInnerHTML={{ __html: theTitle }}
             />
             <p
               className="leading-normal text-2xl mb-8"
-              dangerouslySetInnerHTML={{ __html: text }}
+              dangerouslySetInnerHTML={{ __html: 'text' }}
             />
             {buttonText ? (
               <Link prefetch={false} href={buttonLink ? buttonLink : "#"}>
@@ -42,11 +53,11 @@ export function Hero({
             ) : null}
           </div>
           <div className="w-full md:w-3/5 py-6 text-center">
-            {image && (
+            {'image' && (
               <img
                 className="w-full md:w-4/5 z-50 min-h-500 max-h-500"
                 height={500}
-                src={image}
+                src={'image'}
                 alt={buttonText}
               />
             )}
